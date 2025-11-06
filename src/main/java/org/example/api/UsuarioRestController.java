2b52db60ceac3365c4666ebd53ed96a6a8467607
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-11-06 15:46:30 -0300

    Add ExceptionHandler

--- src/main/java/org/example/api/UsuarioRestController.java
+++ src/main/java/org/example/api/UsuarioRestController.java
@@ -2,12 +2,13 @@ package org.example.api;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -61,5 +62,20 @@ public class UsuarioRestController {
     @DeleteMapping("/{uuid}")
     public void deletarUsuario(@PathVariable UUID uuid){
         this.usuarioList.removeIf(usuario -> usuario.getUuid().equals(uuid));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleValidationExceptions(
+        @NotNull MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+
+        return errors;
     }
 }
