cfea06a61af726879cba4878b871b67bd5ea0f3f
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-11-07 11:39:58 -0300

    Modulariza exceptions

--- src/main/java/org/example/api/exception/ApplicationControllerAdvice.java
+++ src/main/java/org/example/api/exception/ApplicationControllerAdvice.java
@@ -1,17 +1,23 @@
 package org.example.api.exception;
 
+
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @ControllerAdvice
 @Log4j2
@@ -19,17 +25,34 @@ public class ApplicationControllerAdvice extends ResponseEntityExceptionHandler 
 
     public static final String METHOD_ARGUMENT_NOT_VALID_ERROR_MESSAGE = "Campo inválido: '%s'. Causa: '%s'.";
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String,String> handleValidationExceptions(
-            @NotNull MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request){
+        log.error(ex.getMessage(), ex);
+        String errorMessage = getErrorMessages(ex.getBindingResult());
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        final ErrorResponse errorResponse = new ErrorResponse(ex.getClass(), httpStatus, errorMessage);
+        return new ResponseEntity<>(errorResponse,httpStatus);    };
 
-        return errors;
+    private String getErrorMessages(BindingResult bindingResult){
+    return Stream.concat(
+            bindingResult.getFieldErrors().stream().map(this::getMethodArgumentNotValidErrorMessage),
+            bindingResult.getGlobalErrors().stream().map(this::getMethodArgumentNotValidErrorMessage)
+    ).collect(Collectors.joining(", "));
+
+    };
+    private String getMethodArgumentNotValidErrorMessage(@NotNull FieldError error){
+        return  String.format(METHOD_ARGUMENT_NOT_VALID_ERROR_MESSAGE, error.getField(), error.getDefaultMessage());
     }
+
+    private String getMethodArgumentNotValidErrorMessage(@NotNull ObjectError error){
+        return  String.format(METHOD_ARGUMENT_NOT_VALID_ERROR_MESSAGE, error.getObjectName(), error.getDefaultMessage());
+    }
+
+    @ExceptionHandler(value = NaoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> recursoNaoEncontradoExceptionHandler(@NotNull NaoEncontradoException ex){
+        final ErrorResponse errorResponse = new ErrorResponse(ex.getClass(), ex.getStatus(), ex.getMessage());
+        log.debug(ex.getMessage(), ex);
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
+    };
+
 }
--- src/main/java/org/example/api/exception/ErrorResponse.java
+++ src/main/java/org/example/api/exception/ErrorResponse.java
@@ -4,19 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class ErroResponse {
+public class ErrorResponse {
 
     private Class<?> exceptionClass;
     private HttpStatus httpStatus;
     private String message;
     private LocalDateTime thrownAt;
 
-    public  <T> ErroResponse(Class exceptionClass, HttpStatus httpStatus, String message){
+    public  <T> ErrorResponse(Class exceptionClass, HttpStatus httpStatus, String message){
         this.exceptionClass = exceptionClass;
         this.httpStatus = httpStatus;
         this.message = message;
