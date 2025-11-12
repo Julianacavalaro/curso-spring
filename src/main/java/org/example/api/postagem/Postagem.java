db1e7615e09af932e05747784c45e43d4803f619
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-11-11 21:30:16 -0300

    Add schema

--- src/main/java/org/example/api/postagem/Postagem.java
+++ src/main/java/org/example/api/postagem/Postagem.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class Postagem {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private UUID uuid;
--- src/main/java/org/example/api/usuario/Usuario.java
+++ src/main/java/org/example/api/usuario/Usuario.java
@@ -2,6 +2,7 @@ package org.example.api.usuario;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -22,7 +23,7 @@ public class Usuario {
 
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private UUID uuid;
