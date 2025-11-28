7a23492deee6e4e3a464789eff76ee13fa09107f
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-11-28 07:57:02 -0300

    Teste de integração service

--- src/test/java/org/example/api/usuario/UsuarioServiceTest.java
+++ src/test/java/org/example/api/usuario/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package org.example.api.usuario;
 
+import org.example.api.exception.NaoEncontradoException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,14 @@ class UsuarioServiceTest {
     private UsuarioService usuarioService;
 
     @Test
-    void buscarPorUuid() {
+    void sucessoBuscarPorUuid() {
         UUID uuid = UUID.fromString("550e8400-e29b-41d4-a716-446655440002");
         Usuario usuario =  this.usuarioService.buscarPorUuid(uuid);
         Assertions.assertTrue(Objects.nonNull(usuario));
+    }
+    @Test
+    void falhaBuscarPorUuid() {
+        UUID uuid = UUID.fromString("550e8400-e29b-41d4-a716-446655440020");
+        Assertions.assertThrows(NaoEncontradoException.class, () -> this.usuarioService.buscarPorUuid(uuid));
     }
 }
